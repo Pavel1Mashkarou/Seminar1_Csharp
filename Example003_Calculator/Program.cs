21c1ef12eec052812d4f9e6b140bfab735a689ea
Pavel <pmaskarev@vk.com>
2023-03-22 18:53:50 +0300

    Modified calculator

--- Example003_Calculator/Program.cs
+++ Example003_Calculator/Program.cs
@@ -1,5 +1,7 @@
 ﻿int numberA = 3;
 int numberB = 4;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.Write("Результат сложения - ");
 Console.WriteLine(result);
